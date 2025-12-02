0b6c14acf4474a27b3ec1c0f6d4999282a241de7
Enzo Marques 12TA <enzoflashiron@gmail.com>
2025-12-01 21:45:02 -0300

    Outro erro surgiu, ajuda imediata,gerando um exaspero

---
+++ pages/login/login.js
@@ -0,0 +1,37 @@
+const button = document.querySelector("button")
+button.onclick = (event) => {
+    event.preventDefault()
+    login()
+}
+
+async function login() {
+    
+    const email = document.querySelector("#email").value
+    const password = document.querySelector("#password").value
+
+
+    if(email === "" || password === "") {
+        alert("Preencha todas as informações")
+        return
+    }
+
+    const user = {
+        email,
+        password
+    }
+
+    const response = await fetch("http://localhost:3333/login", {
+        method: "POST", 
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({ user })
+
+    }).then(response => response.json())
+
+    console.log(response)
+
+  //  alert(response.message)
+
+//    window.location.href = "../index.html"
+}
